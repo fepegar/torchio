c4b8a77f19b8d5a7c5b024da390415082bd06759
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-28 13:33:48 +0000

    Add Gaussian noise transform

---
+++ torchio/transforms/random_noise.py
@@ -0,0 +1,30 @@
+
+import torch
+
+
+class RandomNoise:
+    def __init__(self, std_range=(0, 0.25), verbose=False):
+        self.std_range = std_range
+        self.verbose = verbose
+
+    def __call__(self, sample):
+        if self.verbose:
+            import time
+            start = time.time()
+        std = self.get_params(self.std_range)
+        sample['random_noise'] = std
+        sample['image'] = add_noise(sample['image'], std)
+        if self.verbose:
+            duration = time.time() - start
+            print(f'RandomNoise: {duration:.1f} seconds')
+        return sample
+
+    @staticmethod
+    def get_params(self, std_range):
+        std = torch.FloatTensor(1).uniform_(*std_range)
+        return std
+
+
+def add_noise(data, std):
+    noise = torch.FloatTensor(*data.shape).normal_(mean=0, std=std)
+    return data + noise
