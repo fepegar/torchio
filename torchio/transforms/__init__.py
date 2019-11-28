0fd45bb258705d2e3975fa152fcd3e740497c2e1
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-28 13:53:09 +0000

    Fix imports

--- torchio/transforms/__init__.py
+++ torchio/transforms/__init__.py
@@ -1,6 +1,5 @@
 from .random_flip import RandomFlip
+from .random_noise import RandomNoise
 from .random_affine import RandomAffine
 from .z_normalization import ZNormalization
 from .histogram_standardisation import HistogramStandardisation
-
-__all__ = ['RandomFlip', 'RandomAffine']
