780593dc880a5a8f5efd33a963724be52bfe4e27
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-28 12:38:50 +0000

    Import intensity transforms

--- torchio/transforms/__init__.py
+++ torchio/transforms/__init__.py
@@ -1,4 +1,6 @@
 from .random_flip import RandomFlip
 from .random_affine import RandomAffine
+from .z_normalization import ZNormalization
+from .histogram_standardisation import HistogramStandardisation
 
 __all__ = ['RandomFlip', 'RandomAffine']
