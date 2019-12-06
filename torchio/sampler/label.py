4850db659806a61439c5d5cdc33834f5533b534d
Fernando Perez-Garcia <fepegar@gmail.com>
2019-12-06 19:42:29 +0000

    Remove unused method

--- torchio/sampler/label.py
+++ torchio/sampler/label.py
@@ -1,17 +1,15 @@
-"""
-This iterable dataset yields patches that contain at least one voxel without
-background. See main() for an example.
-
-For now, this implementation is not efficient because it uses brute force to
-look for foreground voxels.
-"""
-
-import torch
-
 from .sampler import ImageSampler
 
 
 class LabelSampler(ImageSampler):
+    """
+    This iterable dataset yields patches that contain at least one voxel without
+    background. See main() for an example.
+
+    For now, this implementation is not efficient because it uses brute force to
+    look for foreground voxels.
+    """
+    # pylint: disable=abstract-method
     def extract_patch_generator(self, sample, patch_size):
         while True:
             yield self.extract_patch(sample, patch_size)
@@ -29,10 +27,3 @@ class LabelSampler(ImageSampler):
             index_fin,
         )
         return cropped_sample
-
-    @staticmethod
-    def get_data_bounds(label: torch.Tensor):
-        if label.dims() != 4:
-            raise NotImplementedError('Only 3D images is implemented')
-        if label.shape[1] > 1:
-            raise NotImplementedError('Only labelmaps support is implemented')
