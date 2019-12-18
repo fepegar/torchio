3a11db11a01f3f3acc5b303a1833263a3b4761a5
Fernando Perez-Garcia <fepegar@gmail.com>
2019-12-18 18:29:29 +0000

    Add Rescale transform

--- torchio/transforms/__init__.py
+++ torchio/transforms/__init__.py
@@ -1,6 +1,10 @@
+# Spatial transforms
 from .random_flip import RandomFlip
 from .random_noise import RandomNoise
 from .random_affine import RandomAffine
 from .interpolation import Interpolation
+
+# Intensity transforms
+from .rescale import Rescale
 from .z_normalization import ZNormalization
 from .histogram_standardisation import HistogramStandardisation
