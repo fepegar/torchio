2f2b7673358688583a884afbf04a36769a8fdc78
Fernando Perez-Garcia <fepegar@gmail.com>
2019-12-21 01:00:59 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/torchio',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
 )
--- torchio/__init__.py
+++ torchio/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fernando Perez-Garcia"""
 __email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from . import utils
 from . import sampler
