99b4bcd59018eea712ad1c4c0a7714b58ff52608
Fernando Perez-Garcia <fepegar@gmail.com>
2019-12-27 18:34:47 +0000

    Add second image to example

--- examples/example_motion.py
+++ examples/example_motion.py
@@ -1,7 +1,11 @@
 import nibabel as nib
 from torchio import ImagesDataset, transforms, INTENSITY
 
-paths = [{'t1': dict(path='~/Dropbox/MRI/t1.nii.gz', type=INTENSITY)}]
+paths = [{
+    't1': dict(path='~/Dropbox/MRI/t1.nii.gz', type=INTENSITY),
+    'colin': dict(path='/tmp/colin27_t1_tal_lin.nii.gz', type=INTENSITY),
+}]
+
 dataset = ImagesDataset(paths)
 sample = dataset[0]
 transform = transforms.RandomMotion(
@@ -11,7 +15,13 @@ transform = transforms.RandomMotion(
     num_transforms=2,
 )
 transformed = transform(sample)
+
 nib.Nifti1Image(
     transformed['t1']['data'].squeeze(),
     transformed['t1']['affine'],
-).to_filename('/tmp/move.nii.gz')
+).to_filename('/tmp/t1_motion.nii.gz')
+
+nib.Nifti1Image(
+    transformed['colin']['data'].squeeze(),
+    transformed['colin']['affine'],
+).to_filename('/tmp/colin_motion.nii.gz')
