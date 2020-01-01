26ebc053a9ae3cc60dda70c30f82a072ac0216a7
Fernando Perez-Garcia <fepegar@gmail.com>
2020-01-01 20:13:36 +0000

    Add random bias field transform

--- torchio/transforms/__init__.py
+++ torchio/transforms/__init__.py
@@ -3,6 +3,7 @@ from .random_flip import RandomFlip
 from .random_noise import RandomNoise
 from .random_affine import RandomAffine
 from .random_motion import RandomMotion
+from .random_bias_field import RandomBiasField
 from .random_elastic_deformation import RandomElasticDeformation
 from .interpolation import Interpolation
 
