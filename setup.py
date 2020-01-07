3f2b9d8de8c43c807ae7f6d85c4c86da9833ef5f
Fernando Perez-Garcia <fepegar@gmail.com>
2020-01-07 17:27:10 +0000

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/torchio',
-    version='0.7.1',
+    version='0.7.2',
     zip_safe=False,
 )
