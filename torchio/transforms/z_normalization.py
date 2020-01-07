9ca546aea33638933e401ce8d54f46628e5d4398
Fernando Perez-Garcia <fepegar@gmail.com>
2020-01-07 17:27:00 +0000

    Normalize intensity images only

--- torchio/transforms/z_normalization.py
+++ torchio/transforms/z_normalization.py
@@ -1,13 +1,19 @@
+from ..torchio import INTENSITY
 from ..utils import is_image_dict
 from .transform import Transform
 
 
 class ZNormalization(Transform):
+    """
+    Subtract mean and divide by standard deviation
+    """
     def apply_transform(self, sample):
         for image_dict in sample.values():
             if not is_image_dict(image_dict):
                 continue
-            znorm(image_dict['data'])
+            if image_dict['type'] != INTENSITY:
+                continue
+            image_dict['data'] = znorm(image_dict['data'])
         return sample
 
 
@@ -17,8 +23,9 @@ def znorm(data, masking_function=None):
     mask_data = masking_function(data)
     values = data[mask_data]
     mean, std = values.mean(), values.std()
-    data -= mean
-    data /= std
+    data = data - mean
+    data = data / std
+    return data
 
 
 def mean_plus(data):
