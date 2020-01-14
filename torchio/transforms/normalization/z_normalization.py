d7dbc8d4db2ad64bbac22d86dd2b1e992060c70b
Fernando Pérez-García <fepegar@gmail.com>
2020-01-14 10:14:19 +0000

    Increase masking flexibility for normalization (#39)
    * Improve masking flexibility for normalization
    
    * Fix imports
    
    * Add docstring for masking_method

--- torchio/transforms/normalization/z_normalization.py
+++ torchio/transforms/normalization/z_normalization.py
@@ -1,42 +1,24 @@
-import torch
-from ...torchio import INTENSITY
-from ...utils import is_image_dict
-from .. import Transform
+from .normalization_transform import NormalizationTransform
 
 
-class ZNormalization(Transform):
+class ZNormalization(NormalizationTransform):
     """
     Subtract mean and divide by standard deviation
     """
-    def __init__(self, use_mean_threshold=True, verbose=False):
-        super().__init__(verbose=verbose)
-        self.use_mean_threshold = use_mean_threshold
+    def __init__(self, masking_method=None, verbose=False):
+        super().__init__(masking_method=masking_method, verbose=verbose)
 
-    def apply_transform(self, sample):
-        masking_function = mean_plus if self.use_mean_threshold else None
-        for image_dict in sample.values():
-            if not is_image_dict(image_dict):
-                continue
-            if image_dict['type'] != INTENSITY:
-                continue
-            image_dict['data'] = znorm(
-                image_dict['data'],
-                masking_function=masking_function,
-            )
-        return sample
+    def apply_normalization(self, sample, image_name, mask):
+        image_dict = sample[image_name]
+        image_dict['data'] = self.znorm(
+            image_dict['data'],
+            mask,
+        )
 
-
-def znorm(data, masking_function):
-    if masking_function is None:
-        mask_data = torch.ones_like(data, dtype=torch.bool)
-    else:
-        mask_data = masking_function(data)
-    values = data[mask_data]
-    mean, std = values.mean(), values.std()
-    data = data - mean
-    data = data / std
-    return data
-
-
-def mean_plus(data):
-    return data > data.mean()
+    @staticmethod
+    def znorm(data, mask):
+        values = data[mask]
+        mean, std = values.mean(), values.std()
+        data = data - mean
+        data = data / std
+        return data
