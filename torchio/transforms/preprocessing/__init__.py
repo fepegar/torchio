f8c91166947beb3b7258d1858f19e1c6894fc057
Fernando Perez-Garcia <fepegar@gmail.com>
2020-01-14 19:34:20 +0000

    Rename module to preprocessing

--- torchio/transforms/preprocessing/__init__.py
+++ torchio/transforms/preprocessing/__init__.py
@@ -1,4 +1,5 @@
 from .normalization_transform import NormalizationTransform
+from .pad import Pad
 from .rescale import Rescale
 from .z_normalization import ZNormalization
 from .histogram_standardization import HistogramStandardization
