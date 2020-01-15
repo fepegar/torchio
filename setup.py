fe255baaa3263ead7ba9ad2fe84cd6b7694ef52f
Fernando Perez-Garcia <fepegar@gmail.com>
2020-01-15 00:45:21 +0000

    Fix version

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/torchio',
-    version='version='0.11.1'',
+    version='0.11.1',
     zip_safe=False,
 )
