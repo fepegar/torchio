bdacc1b60329a4054ee708743d209aa7234f44fd
valabregue <romain.valabregue@upmc.fr>
2020-01-21 09:37:32 +0100

    change path

--- torchio/transforms/preprocessing/__init__.py
+++ torchio/transforms/preprocessing/__init__.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-from .normalization_transform import NormalizationTransform
-from .pad import Pad
-from .crop import Crop
-from .rescale import Rescale
-from .resample import Resample
-from .z_normalization import ZNormalization
-from .histogram_standardization import HistogramStandardization
-from .histogram_equalize import HistogramEqualize
-from .histogram_random_change import HistogramRandomChange
-=======
 from .spatial.pad import Pad
 from .spatial.crop import Crop
 from .spatial.resample import Resample
@@ -17,4 +6,7 @@ from .spatial.to_canonical import ToCanonical
 from .intensity.rescale import Rescale
 from .intensity.z_normalization import ZNormalization
 from .intensity.histogram_standardization import HistogramStandardization
->>>>>>> 6080a6fd2793244e5552414c4a0de6cb328c75be
+
+from .intensity.histogram_equalize import HistogramEqualize
+from .intensity.histogram_random_change import HistogramRandomChange
+
