4e3e16bb2438e4b5638b3328d1e91c7e3b5d80b9
valabregue <romain.valabregue@upmc.fr>
2020-02-20 16:58:02 +0100

    testing combine_axis

--- examples/example_time_course_kspace_affines.py
+++ examples/example_time_course_kspace_affines.py
@@ -15,28 +15,31 @@ subject = [[
      ]]
 subjects_list = [subject]
 dataset = ImagesDataset(subject)
-sample_orig = dataset[0]
+sample = dataset[0]
 #sample = deepcopy(sample_orig)
-sample = sample_orig
 
 nT = 100
-time_points = [.58, 1.0]
-dim_modif = 1
+time_points = [.55, 1.0]
 fitpars = np.zeros((6, nT))
 
-fitpars[0, 58:] = -94
-fitpars[1, 58:] = -5
-fitpars[2, 58:] = -88
+fitpars[1, 55:] = -15
 #fitpars[dim_modif, :45] = -7.5
 #fitpars[dim_modif, 45:] = 7.5
 
 #ov(sample["T1"]["data"][0], sample["T1"]["affine"])
 
-transform = RandomMotionTimeCourseAffines(fitpars=fitpars, time_points=time_points, pct_oversampling=0.30, verbose=True)
-#transform = RandomMotionTimeCourseAffines(fitpars=fitpars, time_points=time_points, pct_oversampling=0.30, verbose=True,combine_axis=0)
-
+transform = RandomMotionTimeCourseAffines(fitpars=fitpars, time_points=time_points, pct_oversampling=0.30, verbose=True,combine_axis=0)
 transformed = transform(sample)
+dataset.save_sample(transformed, dict(T1='/home/romain.valabregue/tmp/mot/t1_motion_axis0.nii.gz'))
 
-dataset.save_sample(transformed, dict(T1='/home/romain.valabregue/tmp/mot/t1_motion.nii.gz'))
+sample = dataset[0]
+transform = RandomMotionTimeCourseAffines(fitpars=fitpars, time_points=time_points, pct_oversampling=0.30, verbose=True,combine_axis=1)
+transformed = transform(sample)
+dataset.save_sample(transformed, dict(T1='/home/romain.valabregue/tmp/mot/t1_motion_axis1.nii.gz'))
+
+sample = dataset[0]
+transform = RandomMotionTimeCourseAffines(fitpars=fitpars, time_points=time_points, pct_oversampling=0.30, verbose=True,combine_axis=2)
+transformed = transform(sample)
+dataset.save_sample(transformed, dict(T1='/home/romain.valabregue/tmp/mot/t1_motion_axis2.nii.gz'))
 
 #ov(transformed["T1"]["data"][0], sample["T1"]["affine"])
