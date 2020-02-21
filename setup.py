b1b163cebf14764c99a3e6622150657f0350efcc
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2020-02-21 13:49:46 +0000

    Remove generic Python 3 from classifiers

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
