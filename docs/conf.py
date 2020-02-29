3ecad652f693f5b27f5c6b202bd710959fcf9638
Fernando Perez-Garcia <fepegar@gmail.com>
2020-02-29 14:20:16 +0000

    Remove import from docs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import torchio
-import sphinx_rtd_theme
 
 # -- General configuration ---------------------------------------------
 
