1162f710b9a342aef7683ef8c5802f966cfde273
Fernando Pérez-García <fepegar@gmail.com>
2020-03-11 01:06:56 +0000

    Add link to paper in __init__

--- torchio/__init__.py
+++ torchio/__init__.py
@@ -9,3 +9,9 @@ from .torchio import *
 from .transforms import *
 from .data import io, sampler, inference, ImagesDataset, Image, Queue, Subject
 from . import datasets
+
+print('If you use TorchIO for your research, please cite the following paper:')
+print('\nPérez-García et al., TorchIO: a Python library for efficient loading,')
+print('preprocessing, augmentation and patch-based sampling of medical images')
+print('in deep learning')
+print('(https://arxiv.org/abs/2003.04696)')
