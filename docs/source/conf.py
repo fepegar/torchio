20ca10c039fa68372d19469fac3cfad1b26dccb4
Fernando Perez-Garcia <fepegar@gmail.com>
2020-03-15 13:06:09 +0000

    Bump version: 0.13.15 → 0.13.16

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Fernando Pérez-García'
 
 # version is the short X.Y version
 # release is the full version, including alpha/beta/rc tags
-version = release = '0.13.15'
+version = release = '0.13.16'
 
 
 # -- General configuration ---------------------------------------------------
