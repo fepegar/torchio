6da6585e1415463b518df1d6caa454e80e849331
Fernando Perez-Garcia <fepegar@gmail.com>
2020-03-28 17:49:03 +0000

    Add some tests for CLI tool

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,24 +9,38 @@ from .utils import TorchioTestCase
 
 class TestCLI(TorchioTestCase):
     """Tests for CLI tool."""
-
-    def test_command_line_interface(self):
+    def test_help(self):
         """Test the CLI."""
         runner = CliRunner()
-        args = [
-            str(self.get_image_path('t1')),
-            'Rescale',
-            '/tmp/test.nii',
-            # '-k', r'"out_min_max=(0,1) percentiles=(10,90)"',  # not working
-            '-k', 'out_min_max=(-1,1)',
-        ]
-        result = runner.invoke(cli.apply_transform, args)
-        assert result.exit_code == 0
         help_result = runner.invoke(cli.apply_transform, ['--help'])
         assert help_result.exit_code == 0
         assert 'Show this message and exit.' in help_result.output
 
-        # # Why doesn't this work?
-        # args = [str(self.path), 'NoExist', '/tmp/test.nii']
-        # with self.assertRaises(AttributeError):
-        #     result = runner.invoke(cli.apply_transform, args)
+    def test_wrong_transform(self):
+        runner = CliRunner()
+        args = [str(self.get_image_path('image')), 'NoExist', '/tmp/test.nii']
+        print(' '.join(args))
+        result = runner.invoke(cli.apply_transform, args)
+        assert result.exit_code == 1
+
+    def test_wrong_kwargs(self):
+        runner = CliRunner()
+        args = [
+            str(self.get_image_path('image')),
+            'Rescale',
+            '/tmp/test.nii',
+            '-k', '"out_min_max=(0,1),percentiles=(10,90)"'
+        ]
+        result = runner.invoke(cli.apply_transform, args)
+        assert result.exit_code == 1
+
+    def test_valid_kwargs(self):
+        runner = CliRunner()
+        args = [
+            str(self.get_image_path('image')),
+            'Rescale',
+            '/tmp/test.nii',
+            '-k', '"out_min_max=(0,1) percentiles=(10,90)"'
+        ]
+        result = runner.invoke(cli.apply_transform, args)
+        assert result.exit_code == 0
