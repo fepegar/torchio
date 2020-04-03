e6689a594911b8b84c30a6c675404b0119fc89de
Fernando Perez-Garcia <fepegar@gmail.com>
2020-04-03 16:24:06 +0100

    Reformat citation message

--- torchio/__init__.py
+++ torchio/__init__.py
@@ -12,7 +12,6 @@ from . import datasets
 from . import reference
 
 print('If you use TorchIO for your research, please cite the following paper:')
-print('\nPérez-García et al., TorchIO: a Python library for efficient loading,')
+print('Pérez-García et al., TorchIO: a Python library for efficient loading,')
 print('preprocessing, augmentation and patch-based sampling of medical images')
-print('in deep learning')
-print('(https://arxiv.org/abs/2003.04696)')
+print('in deep learning. Link: https://arxiv.org/abs/2003.04696\n')
