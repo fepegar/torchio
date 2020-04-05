e8235f1e0214915af9d24dd3a905f379b458e7ec
Fernando Pérez-García <fepegar@gmail.com>
2020-04-05 23:19:31 +0100

    Add duecredit (#114)
    * Add duecredit items
    
    * Add path to arguments
    
    * Reformat citation message
    
    * Add cite_module=True to credit on import

--- torchio/__init__.py
+++ torchio/__init__.py
@@ -9,9 +9,9 @@ from .torchio import *
 from .transforms import *
 from .data import io, sampler, inference, ImagesDataset, Image, Queue, Subject
 from . import datasets
+from . import reference
 
 print('If you use TorchIO for your research, please cite the following paper:')
-print('\nPérez-García et al., TorchIO: a Python library for efficient loading,')
+print('Pérez-García et al., TorchIO: a Python library for efficient loading,')
 print('preprocessing, augmentation and patch-based sampling of medical images')
-print('in deep learning')
-print('(https://arxiv.org/abs/2003.04696)')
+print('in deep learning. Link: https://arxiv.org/abs/2003.04696\n')
