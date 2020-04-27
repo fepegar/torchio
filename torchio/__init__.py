82f3c714819526aee78292d31ff2e3d074d46881
Fernando Perez-Garcia <fepegar@gmail.com>
2020-04-27 14:09:43 +0100

    Bump version: 0.16.3 → 0.16.4

--- torchio/__init__.py
+++ torchio/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fernando Perez-Garcia"""
 __email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
-__version__ = '0.16.3'
+__version__ = '0.16.4'
 
 from . import utils
 from .torchio import *
