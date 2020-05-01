138f68423ba658d9a0ff2f01fd77b2f74257c5d0
Fernando Perez-Garcia <fepegar@gmail.com>
2020-05-01 10:04:23 +0100

    Update heteromodal example

--- examples/example_heteromodal.py
+++ examples/example_heteromodal.py
@@ -13,61 +13,60 @@ from torch.utils.data import DataLoader
 import torchio
 from torchio import Image, Subject, ImagesDataset, Queue
 from torchio.data import ImageSampler
-from torchio.transforms import (
-    ZNormalization,
-    RandomNoise,
-    RandomFlip,
-    RandomAffine,
-)
 
-# Define training and patches sampling parameters
-num_epochs = 4
-patch_size = 128
-queue_length = 100
-samples_per_volume = 1
-batch_size = 2
+def main():
+    # Define training and patches sampling parameters
+    num_epochs = 20
+    patch_size = 128
+    queue_length = 100
+    samples_per_volume = 5
+    batch_size = 2
 
-# Populate a list with images
-one_subject = Subject(
-    T1=Image(../BRATS2018_crop_renamed/LGG75_T1.nii.gz', torchio.INTENSITY),
-    T2=Image('../BRATS2018_crop_renamed/LGG75_T2.nii.gz', torchio.INTENSITY),
-    label=Image('../BRATS2018_crop_renamed/LGG75_Label.nii.gz', torchio.LABEL),
-)
+    # Populate a list with images
+    one_subject = Subject(
+        T1=Image('../BRATS2018_crop_renamed/LGG75_T1.nii.gz', torchio.INTENSITY),
+        T2=Image('../BRATS2018_crop_renamed/LGG75_T2.nii.gz', torchio.INTENSITY),
+        label=Image('../BRATS2018_crop_renamed/LGG75_Label.nii.gz', torchio.LABEL),
+    )
 
-# This subject doesn't have a T2 MRI!
-another_subject = Subject(
-    T1=Image(../BRATS2018_crop_renamed/LGG74_T1.nii.gz', torchio.INTENSITY),
-    label=Image('../BRATS2018_crop_renamed/LGG74_Label.nii.gz', torchio.LABEL),
-)
+    # This subject doesn't have a T2 MRI!
+    another_subject = Subject(
+        T1=Image('../BRATS2018_crop_renamed/LGG74_T1.nii.gz', torchio.INTENSITY),
+        label=Image('../BRATS2018_crop_renamed/LGG74_Label.nii.gz', torchio.LABEL),
+    )
 
-subjects = [
-    one_subject,
-    another_subject,
-]
+    subjects = [
+        one_subject,
+        another_subject,
+    ]
 
-subjects_dataset = ImagesDataset(subjects)
-queue_dataset = Queue(
-    subjects_dataset,
-    queue_length,
-    samples_per_volume,
-    patch_size,
-    ImageSampler,
-)
+    subjects_dataset = ImagesDataset(subjects)
+    queue_dataset = Queue(
+        subjects_dataset,
+        queue_length,
+        samples_per_volume,
+        patch_size,
+        ImageSampler,
+    )
 
-# This collate_fn is needed in the case of missing modalities
-# In this case, the batch will be composed by a *list* of samples instead of
-# the typical Python dictionary that is collated by default in Pytorch
-batch_loader = DataLoader(
-    queue_dataset,
-    batch_size=batch_size,
-    collate_fn=lambda x: x,
-)
+    # This collate_fn is needed in the case of missing modalities
+    # In this case, the batch will be composed by a *list* of samples instead of
+    # the typical Python dictionary that is collated by default in Pytorch
+    batch_loader = DataLoader(
+        queue_dataset,
+        batch_size=batch_size,
+        collate_fn=lambda x: x,
+    )
 
-# Mock PyTorch model
-model = nn.Module()
+    # Mock PyTorch model
+    model = nn.Identity()
 
-for epoch_index in range(num_epochs):
-    for batch in batch_loader:  # batch is a *list* here, not a dictionary
-        logits = model(batch)
-        print([batch[idx].keys() for idx in range(batch_size)])
-print()
+    for epoch_index in range(num_epochs):
+        for batch in batch_loader:  # batch is a *list* here, not a dictionary
+            logits = model(batch)
+            print([batch[idx].keys() for idx in range(batch_size)])
+    print()
+
+
+if __name__ == "__main__":
+    main()
