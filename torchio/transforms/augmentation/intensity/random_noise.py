5a2830c9e1ed838db4dadee68aabc20cdc7e0765
Fernando <fepegar@gmail.com>
2020-05-03 17:11:39 +0100

    Add mean argument to RandomNoise

--- torchio/transforms/augmentation/intensity/random_noise.py
+++ torchio/transforms/augmentation/intensity/random_noise.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union
 import torch
 import numpy as np
 from ....torchio import DATA
@@ -10,20 +10,26 @@ class RandomNoise(RandomTransform):
     r"""Add random Gaussian noise.
 
     Args:
+        mean: Mean :math:`\mu` of the Gaussian distribution
+            from which the noise is sampled.
+            If two values :math:`(a, b)` are provided,
+            then :math:`\mu \sim \mathcal{U}(a, b)`.
         std: Standard deviation :math:`\sigma` of the Gaussian distribution
             from which the noise is sampled.
-            If two values :math:`(a, b)` are providede,
+            If two values :math:`(a, b)` are provided,
             then :math:`\sigma \sim \mathcal{U}(a, b)`.
         p: Probability that this transform will be applied.
         seed: See :py:class:`~torchio.transforms.augmentation.RandomTransform`.
     """
     def __init__(
             self,
-            std: Tuple[float, float] = (0, 0.25),
+            mean: Union[float, Tuple[float, float]] = 0,
+            std: Union[float, Tuple[float, float]] = (0, 0.25),
             p: float = 1,
             seed: Optional[int] = None,
             ):
         super().__init__(p=p, seed=seed)
+        self.mean_range = self.parse_range(mean, 'mean')
         self.std_range = self.parse_range(std, 'std')
         if any(np.array(self.std_range) < 0):
             message = (
@@ -35,20 +41,24 @@ class RandomNoise(RandomTransform):
     def apply_transform(self, sample: Subject) -> dict:
         random_parameters_images_dict = {}
         for image_name, image_dict in sample.get_images_dict().items():
-            std = self.get_params(self.std_range)
+            mean, std = self.get_params(self.mean_range, self.std_range)
             random_parameters_dict = {'std': std}
             random_parameters_images_dict[image_name] = random_parameters_dict
-            image_dict[DATA] = add_noise(image_dict[DATA], std)
+            image_dict[DATA] = add_noise(image_dict[DATA], mean, std)
         sample.add_transform(self, random_parameters_images_dict)
         return sample
 
     @staticmethod
-    def get_params(std_range: Tuple[float, float]) -> float:
+    def get_params(
+            mean_range: Tuple[float, float],
+            std_range: Tuple[float, float],
+            ) -> Tuple[float, float]:
+        mean = torch.FloatTensor(1).uniform_(*mean_range).item()
         std = torch.FloatTensor(1).uniform_(*std_range).item()
-        return std
+        return mean, std
 
 
-def add_noise(tensor: torch.Tensor, std: float) -> torch.Tensor:
-    noise = torch.FloatTensor(*tensor.shape).normal_(mean=0, std=std)
+def add_noise(tensor: torch.Tensor, mean: float, std: float) -> torch.Tensor:
+    noise = torch.FloatTensor(*tensor.shape).normal_(mean=mean, std=std)
     tensor = tensor + noise
     return tensor
