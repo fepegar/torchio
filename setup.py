1a1240f5e115f3be1b883e402ee238b4a62ec281
Fernando <fepegar@gmail.com>
2020-05-03 17:11:48 +0100

    Bump version: 0.16.4 → 0.16.5

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=[],
     url='https://github.com/fepegar/torchio',
-    version='0.16.4',
+    version='0.16.5',
     zip_safe=False,
 )
--- torchio/__init__.py
+++ torchio/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fernando Perez-Garcia"""
 __email__ = 'fernando.perezgarcia.17@ucl.ac.uk'
-__version__ = '0.16.4'
+__version__ = '0.16.5'
 
 from . import utils
 from .torchio import *
