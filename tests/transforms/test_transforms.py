20f104973eb5096bbd1488a45d68f38a75d27ab4
Fernando <fepegar@gmail.com>
2020-05-23 10:20:50 +0100

    Add 2D transforms to tests

--- tests/transforms/test_transforms.py
+++ tests/transforms/test_transforms.py
@@ -9,16 +9,16 @@ from ..utils import TorchioTestCase
 class TestTransforms(TorchioTestCase):
     """Tests for all transforms."""
 
-    def get_transform(self, channels):
+    def get_transform(self, channels, is_3d=True):
         landmarks_dict = {
             channel: np.linspace(0, 100, 13) for channel in channels
         }
         elastic = torchio.RandomElasticDeformation(max_displacement=1)
         transforms = (
-            torchio.CropOrPad((9, 21, 30)),
+            torchio.CropOrPad((9, 21, 30)) if is_3d else torchio.CropOrPad((1, 21, 30)),
             torchio.ToCanonical(),
             torchio.Resample((1, 1.1, 1.25)),
-            torchio.RandomFlip(axes=(0, 1, 2), flip_probability=1),
+            torchio.RandomFlip(axes=(0, 1, 2), flip_probability=1) if is_3d else torchio.RandomFlip(axes=(0, 1), flip_probability=1),
             torchio.RandomMotion(),
             torchio.RandomGhosting(axes=(0, 1, 2)),
             torchio.RandomSpike(),
@@ -38,11 +38,16 @@ class TestTransforms(TorchioTestCase):
         )
         return torchio.Compose(transforms)
 
-    def test_transforms_sample(self):
-        transform = self.get_transform(channels=('t1', 't2'))
-        transform(self.sample)
-
     def test_transforms_tensor(self):
         tensor = torch.rand(2, 4, 5, 8)
         transform = self.get_transform(channels=('channel_0', 'channel_1'))
         transform(tensor)
+
+    def test_transforms_sample_3d(self):
+        transform = self.get_transform(channels=('t1', 't2'))
+        transform(self.sample)
+
+    def test_transforms_sample_2d(self):
+        transform = self.get_transform(channels=('t1', 't2'))
+        sample = self.make_2d(self.sample)
+        transform(sample)
