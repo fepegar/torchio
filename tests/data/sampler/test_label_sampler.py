d4962a5c4a5352e92e2b4a1c4bb36b2771f21d10
Fernando <fepegar@gmail.com>
2020-06-02 00:52:22 +0100

    Add low-level test for label sampler

--- tests/data/sampler/test_label_sampler.py
+++ tests/data/sampler/test_label_sampler.py
@@ -1,4 +1,5 @@
-from torchio import DATA
+import torch
+import torchio
 from torchio.data import LabelSampler
 from ...utils import TorchioTestCase
 
@@ -9,4 +10,17 @@ class TestLabelSampler(TorchioTestCase):
     def test_label_sampler(self):
         sampler = LabelSampler(5, 'label')
         for patch in sampler(self.sample, num_patches=10):
-            self.assertEqual(patch['label'][DATA][0, 2, 2, 2], 1)
+            patch_center = patch['label'][torchio.DATA][0, 2, 2, 2]
+            self.assertEqual(patch_center, 1)
+
+    def test_label_probabilities(self):
+        labels = torch.Tensor((0, 0, 1, 1, 2, 1, 0)).reshape(1, 1, -1)
+        subject = torchio.Subject(
+            label=torchio.Image(tensor=labels, type=torchio.LABEL),
+        )
+        sample = torchio.ImagesDataset([subject])[0]
+        probs_dict = {0: 0, 1: 50, 2: 25, 3: 25}
+        sampler = LabelSampler(5, 'label', label_probabilities=probs_dict)
+        probabilities = sampler.get_probability_map(sample)
+        fixture = torch.Tensor((0, 0, 2/12, 2/12, 3/12, 2/12, 0))
+        assert torch.all(probabilities.squeeze().eq(fixture))
