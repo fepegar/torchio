d46a43a52c97ce12904ac339b1a336ab299fd68d
Fernando <fepegar@gmail.com>
2020-07-09 15:59:46 +0100

    Round labels of Colin27 label map

--- torchio/datasets/mni/colin.py
+++ torchio/datasets/mni/colin.py
@@ -1,15 +1,25 @@
 import urllib.parse
 from torchvision.datasets.utils import download_and_extract_archive
 from ...utils import get_torchio_cache_dir
-from ... import Image, LABEL
+from ... import Image, LABEL, DATA
 from .mni import SubjectMNI
 
 
 class Colin27(SubjectMNI):
-    """Colin27 MNI template.
+    r"""Colin27 MNI template.
+
+    More information can be found in the website of the
+    `1998 <http://nist.mni.mcgill.ca/?p=935>`_ and
+    `2008 <http://www.bic.mni.mcgill.ca/ServicesAtlases/Colin27Highres>`_
+    versions.
 
     Arguments:
-        version: Template year. It can ``1998`` or ``2008``.
+        version: Template year. It can be ``1998`` or ``2008``.
+
+    .. warning:: The resolution of the ``2008`` version is quite high. The
+        subject instance will contain four images of size
+        :math:`362 \times 434 \times 362`, therefore applying a transform to
+        it might take longer than expected.
     """
     def __init__(self, version=1998):
         if version not in (1998, 2008):
@@ -43,9 +53,13 @@ class Colin27(SubjectMNI):
                 download_root / f'colin27_{name}_tal_hires.nii'
                 for name in ('t1', 't2', 'pd', 'cls')
             ]
+            # Labels do not seem to be encoded correctly in this file
+            # See https://github.com/fepegar/torchio/issues/220
+            cls_image = Image(label, type=LABEL)
+            cls_image[DATA] = cls_image[DATA].round()
             super().__init__(
                 t1=Image(t1),
                 t2=Image(t2),
                 pd=Image(pd),
-                cls=Image(label, type=LABEL),
+                cls=cls_image,
             )
