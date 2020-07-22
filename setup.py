7a9a90406f78a08784e61f5781a8fe33506e6290
sarthakpati <sarthak.pati@hotmail.com>
2020-07-22 13:38:55 -0400

    fixing encoding

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open('README.md', encoding="utf8") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.rst', encoding="utf8") as history_file:
     history = history_file.read()
 
 requirements = [
