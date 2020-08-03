eced292099edf72d741c9e8bbf5d07608cc9ad9f
Fernando <fepegar@gmail.com>
2020-08-03 17:32:05 +0100

    Bump version: 0.17.17 → 0.17.18

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Fernando Pérez-García'
 
 # version is the short X.Y version
 # release is the full version, including alpha/beta/rc tags
-version = release = '0.17.17'
+version = release = '0.17.18'
 
 
 # -- General configuration ---------------------------------------------------
