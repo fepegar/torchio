887af566285b902734027b5a6ab10b13f7211b8a
GFabien <fabien.girka@supelec.fr>
2020-09-17 15:09:41 +0200

    Update LabelSampler to handle multichannel inputs

--- torchio/data/sampler/label.py
+++ torchio/data/sampler/label.py
@@ -82,15 +82,25 @@ class LabelSampler(WeightedSampler):
             label_probabilities_dict: Dict[int, float],
             ) -> torch.Tensor:
         """Create probability map according to label map probabilities."""
+        multichannel = label_map.shape[0] > 1
         probability_map = torch.zeros_like(label_map)
         label_probs = torch.Tensor(list(label_probabilities_dict.values()))
         normalized_probs = label_probs / label_probs.sum()
         iterable = zip(label_probabilities_dict, normalized_probs)
         for label, label_probability in iterable:
-            mask = label_map == label
+            if multichannel:
+                mask = label_map[label]
+            else:
+                mask = label_map == label
             label_size = mask.sum()
             if not label_size:
                 continue
             prob_voxels = label_probability / label_size
             probability_map[mask] = prob_voxels
+            if multichannel:
+                probability_map[label] = prob_voxels * mask
+            else:
+                probability_map[mask] = prob_voxels
+        if multichannel:
+            probability_map = probability_map.sum(dim=0, keepdim=True)
         return probability_map
