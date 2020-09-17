c10bda41f72cec7b26e4e946bd0b0cb784bddd0f
GFabien <fabien.girka@supelec.fr>
2020-09-17 15:27:00 +0200

    Remove direct calls to numpy.testing functions

--- tests/transforms/augmentation/test_random_gamma.py
+++ tests/transforms/augmentation/test_random_gamma.py
@@ -1,6 +1,5 @@
 from torchio import RandomGamma
 from ...utils import TorchioTestCase
-from numpy.testing import assert_array_almost_equal
 
 
 class TestRandomGamma(TorchioTestCase):
@@ -8,23 +7,26 @@ class TestRandomGamma(TorchioTestCase):
     def test_with_zero_gamma(self):
         transform = RandomGamma(log_gamma_range=0)
         transformed = transform(self.sample)
-        assert_array_almost_equal(self.sample.t1.data, transformed.t1.data)
+        self.assertTensorAlmostEqual(self.sample.t1.data, transformed.t1.data)
 
     def test_with_non_zero_gamma(self):
         transform = RandomGamma(log_gamma_range=(0.1, 0.3))
         transformed = transform(self.sample)
-        with self.assertRaises(AssertionError):
-            assert_array_almost_equal(self.sample.t1.data, transformed.t1.data)
+        self.assertTensorNotEqual(self.sample.t1.data, transformed.t1.data)
 
     def test_with_high_gamma(self):
         transform = RandomGamma(log_gamma_range=(100, 100))
         transformed = transform(self.sample)
-        assert_array_almost_equal(self.sample.t1.data == 1, transformed.t1.data)
+        self.assertTensorAlmostEqual(
+            self.sample.t1.data == 1, transformed.t1.data
+        )
 
     def test_with_low_gamma(self):
         transform = RandomGamma(log_gamma_range=(-100, -100))
         transformed = transform(self.sample)
-        assert_array_almost_equal(self.sample.t1.data > 0, transformed.t1.data)
+        self.assertTensorAlmostEqual(
+            self.sample.t1.data > 0, transformed.t1.data
+        )
 
     def test_wrong_gamma_type(self):
         with self.assertRaises(ValueError):
