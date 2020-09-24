73f545dc2a1b8289ffd2517d2c8115bb67205931
Fernando <fepegar@gmail.com>
2020-09-25 00:00:08 +0100

    Fix anatomical axes in random flip

--- tests/transforms/augmentation/test_random_flip.py
+++ tests/transforms/augmentation/test_random_flip.py
@@ -1,3 +1,4 @@
+import torch
 from torchio import RandomFlip
 from ...utils import TorchioTestCase
 
@@ -9,8 +10,9 @@ class TestRandomFlip(TorchioTestCase):
         transform = RandomFlip(axes=(1, 2), flip_probability=1)
         transformed = transform(sample)
         self.assertTensorEqual(
-            sample.t1.data.numpy()[:, :, ::-1, ::-1],
-            transformed.t1.data.numpy())
+            sample.t1.data.numpy()[..., ::-1, ::-1],
+            transformed.t1.data.numpy(),
+        )
 
     def test_out_of_range_axis(self):
         with self.assertRaises(ValueError):
@@ -27,3 +29,12 @@ class TestRandomFlip(TorchioTestCase):
     def test_wrong_flip_probability_type(self):
         with self.assertRaises(ValueError):
             RandomFlip(flip_probability='wrong')
+
+    def test_anatomical_axis(self):
+        transform = RandomFlip(axes=['i'], flip_probability=1)
+        tensor = torch.rand(1, 2, 3, 4)
+        transformed = transform(tensor)
+        self.assertTensorEqual(
+            tensor.numpy()[..., ::-1],
+            transformed.numpy(),
+        )
--- torchio/transforms/augmentation/spatial/random_flip.py
+++ torchio/transforms/augmentation/spatial/random_flip.py
@@ -56,7 +56,7 @@ class RandomFlip(RandomTransform, SpatialTransform):
         if any(isinstance(n, str) for n in axes):
             sample.check_consistent_orientation()
             image = sample.get_first_image()
-            axes = sorted([4 + image.axis_name_to_index(n) for n in axes])
+            axes = sorted([3 + image.axis_name_to_index(n) for n in axes])
         for i in range(3):
             if i not in axes:
                 axes_to_flip_hot[i] = False
