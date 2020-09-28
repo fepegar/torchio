51bd508254956b84dc1404def45c493ea0524630
Fernando <fepegar@gmail.com>
2020-09-28 16:35:59 +0100

    Bump version: 0.17.44 → 0.17.45

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Fernando Pérez-García'
 
 # version is the short X.Y version
 # release is the full version, including alpha/beta/rc tags
-version = release = '0.17.44'
+version = release = '0.17.45'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=[],
     url='https://github.com/fepegar/torchio',
-    version='0.17.44',
+    version='0.17.45',
     zip_safe=False,
 )
