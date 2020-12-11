729305135bc31bfac033e5a19472f5d195340de1
valabregue <romain.valabregue@upmc.fr>
2020-12-11 10:19:03 +0100

    Fix NaNs after using Pad from history (#378)
    * remove internal self.fill, and keep padding_mode as string or number
    
    * Add test for #376
    
    Co-authored-by: Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>

--- tests/transforms/preprocessing/test_pad.py
+++ tests/transforms/preprocessing/test_pad.py
@@ -1,6 +1,7 @@
+import torch
 import SimpleITK as sitk
+import torchio as tio
 from torchio.utils import sitk_to_nib
-from torchio.transforms import Pad
 from ...utils import TorchioTestCase
 
 
@@ -12,8 +13,13 @@ class TestPad(TorchioTestCase):
         sitk_image = image.as_sitk()
         low, high = padding[::2], padding[1::2]
         sitk_padded = sitk.ConstantPad(sitk_image, low, high, 0)
-        tio_padded = Pad(padding, padding_mode=0)(image)
+        tio_padded = tio.Pad(padding, padding_mode=0)(image)
         sitk_tensor, sitk_affine = sitk_to_nib(sitk_padded)
         tio_tensor, tio_affine = sitk_to_nib(tio_padded.as_sitk())
         self.assertTensorEqual(sitk_tensor, tio_tensor)
         self.assertTensorEqual(sitk_affine, tio_affine)
+
+    def test_nans_history(self):
+        padded = tio.Pad(1, padding_mode=2)(self.sample_subject)
+        again = padded.history[0](self.sample_subject)
+        assert not torch.isnan(again.t1.data).any()
