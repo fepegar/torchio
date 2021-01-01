bed155db837cdcf07e6c9c9d73906a49b915ebd7
Fernando <fepegar@gmail.com>
2021-01-01 14:54:51 +0000

    Add Python 3.9 to CI

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description=(
         'Tools for loading, augmenting and writing 3D medical images'
