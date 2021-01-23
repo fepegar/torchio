ca331fe2d41ae03b71db6bf2c29e490c470dd1f5
Fernando <fepegar@gmail.com>
2021-01-23 10:26:28 +0000

    Stop using lambda within Queue
    Fixes #422.
    Related to #366.

--- torchio/data/queue.py
+++ torchio/data/queue.py
@@ -228,15 +228,20 @@ class Queue(Dataset):
             subject = next(self.subjects_iterable)
         return subject
 
+    @staticmethod
+    def get_first_item(batch):
+        return batch[0]
+
     def get_subjects_iterable(self) -> Iterator:
         # I need a DataLoader to handle parallelism
         # But this loader is always expected to yield single subject samples
         self._print(
-            '\nCreating subjects loader with', self.num_workers, 'workers')
+            f'\nCreating subjects loader with {self.num_workers} workers')
         subjects_loader = DataLoader(
             self.subjects_dataset,
             num_workers=self.num_workers,
-            collate_fn=lambda x: x[0],
+            batch_size=1,
+            collate_fn=self.get_first_item,
             shuffle=self.shuffle_subjects,
         )
         return iter(subjects_loader)
