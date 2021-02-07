2513be890dc028cbc187af0ba730c543b7b0327f
Akis Linardos <linardos.akis@gmail.com>
2021-02-07 12:51:04 +0200

    Add support to use list of masks for histogram standardization (#446)
    * added masks functionality to HistogramStandardization.train
    
    * Add tests and edit docstring
    
    Co-authored-by: Fernando <fepegar@gmail.com>

--- tests/transforms/preprocessing/test_histogram_standardization.py
+++ tests/transforms/preprocessing/test_histogram_standardization.py
@@ -10,33 +10,45 @@ class TestHistogramStandardization(TorchioTestCase):
 
     def setUp(self):
         super().setUp()
-        self.subjects = [
-            Subject(
-                image=ScalarImage(self.get_image_path(f'hs_image_{i}')),
-                label=LabelMap(
-                    self.get_image_path(
-                        f'hs_label_{i}',
-                        binary=True,
-                        force_binary_foreground=True,
-                    ),
-                ),
-            )
-            for i in range(5)
-        ]
+        subjects = []
+        for i in range(5):
+            image = ScalarImage(self.get_image_path(f'hs_image_{i}'))
+            label_path = self.get_image_path(
+                f'hs_label_{i}', binary=True, force_binary_foreground=True)
+            label = LabelMap(label_path)
+            subject = Subject(image=image, label=label)
+            subjects.append(subject)
+        self.subjects = subjects
         self.dataset = SubjectsDataset(self.subjects)
 
     def test_train_histogram(self):
-        paths = [sample['image']['path'] for sample in self.dataset]
+        paths = [subject.image.path for subject in self.dataset]
+        # Use a function to mask
         HistogramStandardization.train(
             paths,
             masking_function=HistogramStandardization.mean,
             output_path=(self.dir / 'landmarks.txt'),
         )
+        # Use a file to mask
         HistogramStandardization.train(
             paths,
-            mask_path=self.dataset[0]['label']['path'],
+            mask_path=self.dataset[0].label.path,
             output_path=(self.dir / 'landmarks.npy'),
         )
+        # Use files to mask
+        masks = [subject.label.path for subject in self.dataset]
+        HistogramStandardization.train(
+            paths,
+            mask_path=masks,
+            output_path=(self.dir / 'landmarks_masks.npy'),
+        )
+
+    def test_bad_paths_lengths(self):
+        with self.assertRaises(ValueError):
+            HistogramStandardization.train(
+                [1, 2],
+                mask_path=[1, 2, 3],
+            )
 
     def test_normalize(self):
         landmarks = np.linspace(0, 100, 13)
