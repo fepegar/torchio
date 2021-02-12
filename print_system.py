9b42f73e1de248f5d20490dd3a36f44b03bd7318
Fernando <fepegar@gmail.com>
2021-02-12 01:01:33 +0000

    Print Python version for bug report

--- print_system.py
+++ print_system.py
@@ -1,10 +1,13 @@
+# flake8: noqa
+
+import sys
 import platform
 import torchio
 import torch
 import numpy
 
-
-print('Platform:', platform.platform())  # noqa: T001
-print('TorchIO: ', torchio.__version__)  # noqa: T001
-print('PyTorch: ', torch.__version__)  # noqa: T001
-print('NumPy:   ', numpy.__version__)  # noqa: T001
+print('Platform:', platform.platform())
+print('TorchIO: ', torchio.__version__)
+print('PyTorch: ', torch.__version__)
+print('NumPy:   ', numpy.__version__)
+print('Python:  ', sys.version)
