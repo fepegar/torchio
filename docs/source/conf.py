a2e5333e9a3e8e24ccbe6b682820034028d43409
Fernando <fepegar@gmail.com>
2021-03-25 15:29:03 +0000

    Add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -98,6 +98,8 @@ pygments_style = None
 #
 html_theme = 'furo'
 
+html_favicon = 'favicon_io/favicon.ico'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
