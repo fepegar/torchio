e60e14cb5cfa79f91296fe3cdc2d0a487f45a1ce
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2021-03-30 15:55:49 +0100

    Add logo to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,6 +99,7 @@ pygments_style = None
 html_theme = 'furo'
 
 html_favicon = 'favicon_io/favicon.ico'
+html_logo = 'favicon_io/android-chrome-512x512.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
