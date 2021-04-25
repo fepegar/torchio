cb61eb8d2a204eeffc3462f4b104fb9e6d205b8c
Fernando <fepegar@gmail.com>
2021-04-25 12:47:19 +0100

    Add function to get first item from iterable

--- tests/data/sampler/test_weighted_sampler.py
+++ tests/data/sampler/test_weighted_sampler.py
@@ -1,5 +1,5 @@
 import torch
-import torchio
+import torchio as tio
 from torchio.data import WeightedSampler
 from ...utils import TorchioTestCase
 
@@ -10,26 +10,26 @@ class TestWeightedSampler(TorchioTestCase):
     def test_weighted_sampler(self):
         subject = self.get_sample((1, 7, 7, 7))
         sampler = WeightedSampler(5, 'prob')
-        patch = next(iter(sampler(subject)))
+        patch = tio.utils.get_first_item(sampler(subject))
         self.assertEqual(tuple(patch['index_ini']), (1, 1, 1))
 
     def get_sample(self, image_shape):
         t1 = torch.rand(*image_shape)
         prob = torch.zeros_like(t1)
         prob[0, 3, 3, 3] = 1
-        subject = torchio.Subject(
-            t1=torchio.ScalarImage(tensor=t1),
-            prob=torchio.ScalarImage(tensor=prob),
+        subject = tio.Subject(
+            t1=tio.ScalarImage(tensor=t1),
+            prob=tio.ScalarImage(tensor=prob),
         )
-        subject = torchio.SubjectsDataset([subject])[0]
+        subject = tio.SubjectsDataset([subject])[0]
         return subject
 
     def test_inconsistent_shape(self):
         # https://github.com/fepegar/torchio/issues/234#issuecomment-675029767
-        subject = torchio.Subject(
-            im1=torchio.ScalarImage(tensor=torch.rand(1, 4, 5, 6)),
-            im2=torchio.ScalarImage(tensor=torch.rand(2, 4, 5, 6)),
+        subject = tio.Subject(
+            im1=tio.ScalarImage(tensor=torch.rand(1, 4, 5, 6)),
+            im2=tio.ScalarImage(tensor=torch.rand(2, 4, 5, 6)),
         )
         patch_size = 2
-        sampler = torchio.data.WeightedSampler(patch_size, 'im1')
+        sampler = tio.data.WeightedSampler(patch_size, 'im1')
         next(sampler(subject))
