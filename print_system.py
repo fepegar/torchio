6f7c80b24272c59c8ad1c0bc0e8c9f3aaaacd376
Fernando <fepegar@gmail.com>
2021-07-01 09:26:50 +0100

    Print SimpleITK version in environment script

--- print_system.py
+++ print_system.py
@@ -1,13 +1,19 @@
 # flake8: noqa
 
+import re
 import sys
 import platform
 import torchio
 import torch
 import numpy
+import SimpleITK as sitk
 
-print('Platform:', platform.platform())
-print('TorchIO: ', torchio.__version__)
-print('PyTorch: ', torch.__version__)
-print('NumPy:   ', numpy.__version__)
-print('Python:  ', sys.version)
+
+sitk_version = re.findall('SimpleITK Version: (.*?)\n', str(sitk.Version()))[0]
+
+print('Platform:  ', platform.platform())
+print('TorchIO:   ', torchio.__version__)
+print('PyTorch:   ', torch.__version__)
+print('SimpleITK: ', sitk_version)
+print('NumPy:     ', numpy.__version__)
+print('Python:    ', sys.version)
