81bb305749930a4c59ac0d81dbea6325f6526303
Fernando <fepegar@gmail.com>
2021-07-05 23:07:16 +0100

    Minor edits in tests

--- tests/transforms/preprocessing/test_copy_affine.py
+++ tests/transforms/preprocessing/test_copy_affine.py
@@ -1,20 +1,19 @@
-from torchio.transforms import CopyAffine
-from ...utils import TorchioTestCase
-import torchio as tio
-import numpy as np
 import torch
+
+import torchio as tio
+from ...utils import TorchioTestCase
 
 
 class TestCopyAffine(TorchioTestCase):
     """Tests for `CopyAffine`."""
 
     def test_missing_reference(self):
-        transform = CopyAffine(target_key='missing')
+        transform = tio.CopyAffine(target='missing')
         with self.assertRaises(KeyError):
             transform(self.sample_subject)
 
     def test_wrong_target_type(self):
-        transform = CopyAffine(target_key=[1])
+        transform = tio.CopyAffine(target=[1])
         with self.assertRaises(TypeError):
             transform(self.sample_subject)
 
@@ -23,7 +22,9 @@ class TestCopyAffine(TorchioTestCase):
         mask = tio.LabelMap(tensor=torch.rand(2, 2, 2, 2))
         mask.affine *= 1.1
         subject = tio.Subject(t1=image, mask=mask)
-        transform = CopyAffine('t1')
+        transform = tio.CopyAffine('t1')
         transformed = transform(subject)
-        self.assertTrue(np.array_equal(transformed['t1'].affine,
-                                       transformed['mask'].affine))
+        self.assertTensorEqual(
+            transformed['t1'].affine,
+            transformed['mask'].affine,
+        )
