fbe5b09c8b5aebd5f144a07aaade5921889986df
Justus Schock <12886177+justusschock@users.noreply.github.com>
2021-07-26 09:36:16 +0200

    Update subject.py

--- torchio/data/subject.py
+++ torchio/data/subject.py
@@ -221,6 +221,42 @@ class Subject(dict):
             relative_tolerance: float = 1e-6,
             absolute_tolerance: float = 1e-6,
             message: Optional[str] = None) -> None:
+        """Checks for consistency of an attribute across all images in the current subject.
+        
+        Args:
+            attribute: The name of the image attribute to check
+            relative_tolerance: The relative tolerance for the consistency check (see formula below)
+            absolute_tolerance: The absolute tolerance for the consistency check (see formula below)
+            message: The error message to be raised if attributes are not consistent.
+            
+        Example:
+            >>> import numpy as np
+            >>> import torch
+            >>> import torchio as tio
+            >>> img = torch.randn(1, 512, 512, 100)
+            >>> mask = torch.tensor(img > 0).type(torch.int16)
+            >>> af1 = np.array([[0.8, 0, 0, 0],
+            ...                 [0, 0.8, 0, 0],
+            ...                 [0, 0, 2.50000000000001, 0],
+            ...                 [0, 0, 0, 1]])
+            >>> af2 = np.array([[0.8, 0, 0, 0],
+            ...                 [0, 0.8, 0, 0],
+            ...                 [0, 0, 2.49999999999999, 0], # small difference here (e.g. due to different reader)
+            ...                 [0, 0, 0, 1]])
+            >>> sub = tio.Subject(
+            ...   image = tio.ScalarImage(tensor = img, affine = af1),
+            ...   mask = tio.LabelMap(tensor = mask, affine = af2)
+            ... )
+            >>> sub.check_consistent_attribute('spacing') # passes due to introduced tolerances
+            
+    
+        
+        Note:
+            As stated in the numpy docs, this computes the following:
+                absolute(a - b) <= (absolute_tolerance + relative_tolerance * absolute(b))
+            with a beeing the attribute of the first image and b being the attributes of all 
+            other images respectively.
+        """
         iterable = self.get_images_dict(intensity_only=False).items()
         if message is None:
             message = (
