123fb3d6eeaffdb10cd270a9903835b3d78c0536
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2021-08-16 19:18:51 +0100

    Minor edits

--- torchio/transforms/preprocessing/intensity/clamp.py
+++ torchio/transforms/preprocessing/intensity/clamp.py
@@ -6,16 +6,21 @@ from ...intensity_transform import IntensityTransform
 
 
 class Clamp(IntensityTransform):
-    """Clamp intensity values to a certain range.
+    """Clamp intensity values into a range :math:`[a, b]`.
+
+    For more information, see :func:`torch.clamp`.
+
     Args:
-        out_min: See :func:`torch.clamp`.
-        out_max: See :func:`torch.clamp`.
+        out_min: Minimum value :math:`a` of the output image. If ``None``, the
+            minimum of the image is used.
+        out_max: Maximum value :math:`b` of the output image. If ``None``, the
+            maximum of the image is used.
 
     Example:
         >>> import torchio as tio
         >>> ct = tio.ScalarImage('ct_scan.nii.gz')
-        >>> ct_air, ct_bone = -1000, 1000
-        >>> clamp = tio.Clamp(out_min=ct_air, out_max=ct_bone)
+        >>> HOUNSFIELD_AIR, HOUNSFIELD_BONE = -1000, 1000
+        >>> clamp = tio.Clamp(out_min=HOUNSFIELD_AIR, out_max=HOUNSFIELD_BONE)
         >>> ct_clamped = clamp(ct)
     """
     def __init__(
@@ -26,13 +31,14 @@ class Clamp(IntensityTransform):
             ):
         super().__init__(**kwargs)
         self.out_min, self.out_max = out_min, out_max
+        self.args_names = 'out_min', 'out_max'
 
     def apply_transform(self, subject: Subject) -> Subject:
         for image in self.get_images(subject):
             self.apply_clamp(image)
         return subject
 
-    def apply_clamp(self, image: ScalarImage):
+    def apply_clamp(self, image: ScalarImage) -> None:
         image.set_data(self.clamp(image.data))
 
     def clamp(self, tensor: torch.Tensor) -> torch.Tensor:
