2a9c091cb4a392e9867399e3c4d9e8a0e8c16ce6
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2021-09-01 19:11:25 +0100

    Improve coverage

--- tests/transforms/preprocessing/test_resize.py
+++ tests/transforms/preprocessing/test_resize.py
@@ -1,3 +1,5 @@
+import torch
+import numpy as np
 import torchio as tio
 from ...utils import TorchioTestCase
 
@@ -17,3 +19,13 @@ class TestResize(TorchioTestCase):
         transformed = transform(self.sample_subject)
         for image in transformed.get_images(intensity_only=False):
             self.assertEqual(image.spatial_shape, target_shape)
+
+    def test_fix_shape(self):
+        # We use values that are known to need cropping
+        tensor = torch.rand(1, 8, 180, 320)
+        affine = np.diag((5, 1, 1, 1))
+        im = tio.ScalarImage(tensor=tensor, affine=affine)
+        target = 12
+        with self.assertWarns(UserWarning):
+            result = tio.Resize(target)(im)
+        self.assertEqual(result.spatial_shape, 3 * (target,))
