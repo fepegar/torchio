e1ae130d0179a708f22bfd6e014e3e6643b0e149
laynr <lhm2005@gmail.com>
2021-10-06 07:02:06 -0600

    remove .axis('off')

--- torchio/visualization.py
+++ torchio/visualization.py
@@ -100,7 +100,6 @@ def plot_volume(
     axi_axis.invert_xaxis()
     axi_axis.set_title('Axial')
     
-    axes.axis('off')
     sag_axis.axis('off')
     cor_axis.axis('off')
     axi_axis.axis('off')
