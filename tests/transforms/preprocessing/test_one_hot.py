b45a8c9156619cd05f9b97fd8f6685d6f03924c6
Fernando Pérez-García <fepegar@gmail.com>
2021-11-18 13:55:42 +0000

    Add logic to invert OneHot

--- tests/transforms/preprocessing/test_one_hot.py
+++ tests/transforms/preprocessing/test_one_hot.py
@@ -14,3 +14,12 @@ class TestOneHot(TorchioTestCase):
         label_map = tio.LabelMap(tensor=torch.rand(2, 3, 3, 3) > 1)
         with self.assertRaises(RuntimeError):
             tio.OneHot()(label_map)
+
+    def test_inverse(self):
+        one_hot = tio.OneHot()
+        subject_one_hot = one_hot(self.sample_subject)
+        subject_back = subject_one_hot.apply_inverse_transform()
+        self.assertTensorEqual(
+            self.sample_subject.label.data,
+            subject_back.label.data,
+        )
--- torchio/transforms/preprocessing/label/one_hot.py
+++ torchio/transforms/preprocessing/label/one_hot.py
@@ -1,5 +1,6 @@
 import torch.nn.functional as F  # noqa: N812
 
+from ....data.image import Image
 from .label_transform import LabelTransform
 
 
@@ -15,17 +16,29 @@ class OneHot(LabelTransform):
         super().__init__(**kwargs)
         self.num_classes = num_classes
         self.args_names = ('num_classes',)
+        self.invert_transform = False
 
     def apply_transform(self, subject):
         for image in self.get_images(subject):
-            if image.num_channels > 1:
-                message = (
-                    'The number of input channels must be 1,'
-                    f' but it is {image.num_channels}'
-                )
-                raise RuntimeError(message)
-            data = image.data[0]
-            num_classes = -1 if self.num_classes is None else self.num_classes
-            one_hot = F.one_hot(data.long(), num_classes=num_classes)
-            image.set_data(one_hot.permute(3, 0, 1, 2).type(data.type()))
+            if self.invert_transform:
+                self.argmax(image)
+            else:
+                self.one_hot(image)
         return subject
+
+    @staticmethod
+    def argmax(image: Image) -> None:
+        data = image.data.argmax(dim=0, keepdim=True)
+        image.set_data(data)
+
+    def one_hot(self, image: Image) -> None:
+        if image.num_channels > 1:
+            message = (
+                'The number of input channels must be 1,'
+                f' but it is {image.num_channels}'
+            )
+            raise RuntimeError(message)
+        data = image.data[0]
+        num_classes = -1 if self.num_classes is None else self.num_classes
+        one_hot = F.one_hot(data.long(), num_classes=num_classes)
+        image.set_data(one_hot.permute(3, 0, 1, 2).type(data.type()))
