Fix computation of kernels in `Blur` transform (#861)
* Fix size of Gaussian kernel for blurring

* Fix typo in docstring

torchio/transforms/augmentation/intensity/random_blur.py
@@ -56,8 +56,8 @@ class Blur(IntensityTransform):
 
     Args:
         std: Tuple :math:`(\sigma_1, \sigma_2, \sigma_3)` representing the
-            the standard deviations (in mm) of the standard deviations
-            of the Gaussian kernels used to blur the image along each axis.
+            the standard deviations (in mm) of the Gaussian kernels used to
+            blur the image along each axis.
         **kwargs: See :class:`~torchio.transforms.Transform` for additional
             keyword arguments.
     """
@@ -91,10 +91,13 @@ class Blur(IntensityTransform):
 def blur(
         data: TypeData,
         spacing: TypeTripletFloat,
-        std_voxel: TypeTripletFloat,
+        std_physical: TypeTripletFloat,
         ) -> torch.Tensor:
     assert data.ndim == 3
-    std_physical = np.array(std_voxel) / np.array(spacing)
-    blurred = ndi.gaussian_filter(data, std_physical)
+    # For example, if the standard deviation of the kernel is 2 mm and the
+    # image spacing is 0.5 mm/voxel, the kernel should be
+    # (2 mm / 0.5 mm/voxel) = 4 voxels wide
+    std_voxel = np.array(std_physical) / np.array(spacing)
+    blurred = ndi.gaussian_filter(data, std_voxel)
     tensor = torch.as_tensor(blurred)
     return tensor