9fad0e852ad4407110c84e43ed9c966f28e8f863
chagelo <changeto104@gmail.com>
2022-09-20 21:02:16 +0800

    Fix fourier

--- src/torchio/transforms/fourier.py
+++ src/torchio/transforms/fourier.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 import torch
 
 
@@ -6,26 +6,12 @@ class FourierTransform:
 
     @staticmethod
     def fourier_transform(tensor: torch.Tensor) -> torch.Tensor:
-        try:
-            import torch.fft
-            transformed = torch.fft.fftn(tensor)
-            fshift = torch.fft.fftshift(transformed)
-            return fshift
-        except ModuleNotFoundError:
-            import torch
-            transformed = np.fft.fftn(tensor)
-            fshift = np.fft.fftshift(transformed)
-            return torch.from_numpy(fshift)
+        transformed = numpy.fft.fftn(tensor)
+        fshift = numpy.fft.fftshift(transformed)
+        return torch.from_numpy(fshift)
 
     @staticmethod
     def inv_fourier_transform(tensor: torch.Tensor) -> torch.Tensor:
-        try:
-            import torch.fft
-            f_ishift = torch.fft.ifftshift(tensor)
-            img_back = torch.fft.ifftn(f_ishift)
-            return img_back
-        except ModuleNotFoundError:
-            import torch
-            f_ishift = np.fft.ifftshift(tensor)
-            img_back = np.fft.ifftn(f_ishift)
-            return torch.from_numpy(img_back)
+        f_ishift = numpy.fft.ifftshift(tensor)
+        img_back = numpy.fft.ifftn(f_ishift)
+        return torch.from_numpy(img_back)
