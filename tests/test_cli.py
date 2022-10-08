71dec9dc1ed63edaa7b95b1553b9866b0b89006d
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-10-08 01:05:50 +0100

    Replace Click with Typer

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,47 +1,40 @@
 #!/usr/bin/env python
 """Tests for CLI tool package."""
-from click.testing import CliRunner
+from typer.testing import CliRunner
 from torchio.cli import apply_transform
 from torchio.cli import print_info
 
 from .utils import TorchioTestCase
 
 
+runner = CliRunner()
+
+
 class TestCLI(TorchioTestCase):
-    """Tests for CLI tool."""
-    def test_help(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        help_result = runner.invoke(apply_transform.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert 'Show this message and exit.' in help_result.output
 
     def test_cli_transform(self):
         image = str(self.get_image_path('cli'))
-        runner = CliRunner()
         args = [
             image,
             'RandomFlip',
             '--seed', '0',
             '--kwargs', 'axes=(0,1,2)',
+            '--hide-progress',
             image,
         ]
-        result = runner.invoke(apply_transform.main, args)
+        result = runner.invoke(apply_transform.app, args)
         assert result.exit_code == 0
-        assert result.output == ''
+        assert result.output.strip() == ''
 
     def test_bad_transform(self):
-        ValueError
         image = str(self.get_image_path('cli'))
-        runner = CliRunner()
         args = [image, 'RandomRandom', image]
-        result = runner.invoke(apply_transform.main, args)
+        result = runner.invoke(apply_transform.app, args)
         assert result.exit_code == 1
 
     def test_cli_hd(self):
         image = str(self.get_image_path('cli'))
-        runner = CliRunner()
         args = [image]
-        result = runner.invoke(print_info.main, args)
+        result = runner.invoke(print_info.app, args)
         assert result.exit_code == 0
         assert result.output == 'ScalarImage(shape: (1, 10, 20, 30); spacing: (1.00, 1.00, 1.00); orientation: RAS+; dtype: torch.DoubleTensor; memory: 46.9 KiB)\n'  # noqa: E501
