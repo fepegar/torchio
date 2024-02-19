c68db8d00855bf23156d5c76da55ef176a471d61
Fernando Pérez-García <fepegar@gmail.com>
2024-02-19 21:37:43 +0000

    Improve readability

--- src/torchio/data/image.py
+++ src/torchio/data/image.py
@@ -672,10 +672,12 @@ class Image(dict):
 
         self.check_is_2d()
         tensor = self.data
+        if len(tensor) not in (1, 3, 4):
+            raise NotImplementedError(
+                'Only 1, 3 or 4 channels are supported for conversion to Pillow image'
+            )
         if len(tensor) == 1:
             tensor = torch.cat(3 * [tensor])
-        if not (len(tensor) == 3 or len(tensor) == 4):
-            raise RuntimeError('The image must have 1, 3 or 4 channels')
         if transpose:
             tensor = tensor.permute(3, 2, 1, 0)
         else:
