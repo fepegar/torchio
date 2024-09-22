e444058f1b19af3a48b47fc40db505600bc2df94
Fernando Pérez-García <fepegar@gmail.com>
2024-09-22 21:27:42 +0100

    Use Ruff for linting

--- print_system.py
+++ print_system.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 import platform
 import re
 import sys
@@ -6,13 +5,13 @@ import sys
 import numpy
 import SimpleITK as sitk
 import torch
-import torchio
 
+import torchio as tio
 
 sitk_version = re.findall('SimpleITK Version: (.*?)\n', str(sitk.Version()))[0]
 
 print('Platform:  ', platform.platform())
-print('TorchIO:   ', torchio.__version__)
+print('TorchIO:   ', tio.__version__)
 print('PyTorch:   ', torch.__version__)
 print('SimpleITK: ', sitk_version)
 print('NumPy:     ', numpy.__version__)
